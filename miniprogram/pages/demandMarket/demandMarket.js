630d389b76bb0739b2d9c467269903855c55768e
zisen123 <1271634703@qq.com>
2019-01-12 17:08:40 +0800

    runnable2

--- miniprogram/pages/demandMarket/demandMarket.js
+++ miniprogram/pages/demandMarket/demandMarket.js
@@ -1,67 +1,7 @@
-//获取公共ui操作类实例
-//const _page = require('../../utils/abstract-page.js');
-//let modCalendar = require('../mod/calendar.js');
-//const models = require('../../data/demo-model.js')
-const util = require('../../utils/util.js')
-
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    listData: []
-  },
-  // methods: uiUtil.getPageMethods(),
-  methods: {
-  },
 
-  goIndex: function () {
+    data:{
 
-    wx.navigateTo({
-      url: '../index/index'
-    })
-  },
-  onShow: function () {
-    global.sss = this;
-    let scope = this;
-  },
-
-  _appendList: function (data) {
-
-    for(let i = 0, len = data.length; i < len; i++) {
-      data[i].dateStr = util.dateUtil.format(new Date(data[i].datetime * 1000), 'H:F' )
     }
 
-    this.setData({
-      listData: this.data.listData.concat(data)
-    });
-  },
-
-  onLoad: function (data) {
-    let scope = this;
-
-    if(!data || !data.sid || !data.aid || !data.date) {
-      this.showToast('参数错误');
-      return
-    }
-
-    this.index = 0;
-    let listModel = models.listModel;
-
-    listModel.setParam({
-      startcityid: data.sid,
-      arrivalcityid: data.aid,
-      startdatetime: data.date / 1000,
-      page: this.index + 1
-    });
-
-    this.showLoading();
-    listModel.execute(function(data) {
-      scope.hideLoading();
-      scope._appendList(data.schedules);
-
-    });
-
-  }
-},
-)
+})
